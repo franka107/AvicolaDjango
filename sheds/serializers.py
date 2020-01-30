67c2c86ac3152fb6e5cfda0945cf19e4932e597f
SQLazo <emequispe@gmail.com>
2020-01-30 09:18:41 -0500

    correccion

--- sheds/serializers.py
+++ sheds/serializers.py
@@ -6,24 +6,24 @@ from farms.models import Farm
 class ShedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shed
-        fields = ('name','farm','created','updated','type')
+        fields = ('id','name','farm','created','updated','type')
 
 class ShedProductionUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShedRegister
-        fields = ('shed','date','food_income','food_deposit','food_consumption','final_deposit','chicken_death','package_total','leftover_eggs','observation')
+        fields = ('id','shed','date','food_income','food_deposit','food_consumption','final_deposit','chicken_death','package_total','leftover_eggs','observation')
 
 class ShedProductionDownSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShedRegister
-        fields = ('shed','date','food_income','food_deposit','food_consumption','final_deposit','chicken_death','package_total','leftover_eggs','observation')
+        fields = ('id','shed','date','food_income','food_deposit','food_consumption','final_deposit','chicken_death','package_total','leftover_eggs','observation')
 
 class ShedRaisedUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShedRegister
-        fields = ('shed','date','food_income','food_deposit','food_consumption','final_deposit','chicken_death','observation')
+        fields = ('id','shed','date','food_income','food_deposit','food_consumption','final_deposit','chicken_death','observation')
 
 class ShedRaisedDownSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShedRegister
-        fields = ('shed','date','food_income','food_deposit','food_consumption','final_deposit','chicken_death','observation')
+        fields = ('id','shed','date','food_income','food_deposit','food_consumption','final_deposit','chicken_death','observation')
