bdb379bf256c53f3311c29d2c6eaf37ceac57f76
SQLazo <emequispe@gmail.com>
2020-02-03 08:40:48 -0500

    Mejores en API

--- sheds/serializers.py
+++ sheds/serializers.py
@@ -3,10 +3,16 @@ from .models import Shed
 from .models import ShedRegister 
 from farms.models import Farm
 
+class FarmSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Farm
+        fields = ('id','name')
+
 class ShedSerializer(serializers.ModelSerializer):
+    farm = FarmSerializer()
     class Meta:
         model = Shed
-        fields = ('id','name','farm','created','updated','type')
+        fields = ('id','name','farm')
 
 class ShedProductionUpSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,9 +25,14 @@ class ShedProductionDownSerializer(serializers.ModelSerializer):
         fields = ('id','shed','date','food_income','food_deposit','food_consumption','final_deposit','chicken_death','package_total','leftover_eggs','observation')
 
 class ShedRaisedUpSerializer(serializers.ModelSerializer):
+    shed = ShedSerializer()
     class Meta:
         model = ShedRegister
         fields = ('id','shed','date','food_income','food_deposit','food_consumption','final_deposit','chicken_death','observation')
+
+    def create(self, validated_data):
+        shed_register = ShedRegister.objects.create(**validated_data)
+        return shed_register
 
 class ShedRaisedDownSerializer(serializers.ModelSerializer):
     class Meta:
